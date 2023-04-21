c918add0cf524708ced22e0d14f111d3392d0a19
Melody0304 <yuqing.ma@edu.SAIT.ca>
2023-04-22 03:44:09 +0800

    添加项目文件。

---
+++ Lab9/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace MyApplication
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<MyEntity> MyEntities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<MyEntity>().HasKey(x => x.Id);
+        }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MyContext())
+            {
+                var entity = new MyEntity { Name = "John" };
+                db.MyEntities.Add(entity);
+                db.SaveChanges();
+
+                var entities = db.MyEntities.ToList();
+                foreach (var e in entities)
+                {
+                    Console.WriteLine("{0} - {1}", e.Id, e.Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
